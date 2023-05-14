84489d8d1e4b15b96ce39951e038e5d879945724
Евгений Меденцев <evmedencev@yandex.ru>
2023-05-15 02:13:01 +0300

    hex-color

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 import datetime as dt
+import webcolors
 
 from .models import Cat, Owner, Achievement, AchievementCat
 
@@ -20,11 +21,30 @@ class AchievementSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
+
+
 class CatSerializer(serializers.ModelSerializer):
     # owner = serializers.StringRelatedField(read_only=True)
     # Убрали read_only=True
     achievements = AchievementSerializer(many=True, required=False)
     age = serializers.SerializerMethodField()
+    color = Hex2NameColor()  # Вот он - наш собственный тип поля
 
     class Meta:
         model = Cat
